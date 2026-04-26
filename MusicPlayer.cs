8c829cb75a4020e8f64c0f0b8b8d93436d4038e7
tbv001 <sebuahakun123@gmail.com>
2026-04-26 17:35:30 +0700

    Version bump

--- MusicPlayer.cs
+++ MusicPlayer.cs
@@ -13,7 +13,7 @@ public class MusicPlayer : BaseUnityPlugin
     internal static new ManualLogSource Logger;
     public const string PLUGIN_GUID = "com.theblackvoid.musicplayer";
     public const string PLUGIN_NAME = "Music Player";
-    public const string PLUGIN_VERSION = "1.0.0";
+    public const string PLUGIN_VERSION = "1.1.0";
     public Harmony HarmonyInstance = new Harmony(PLUGIN_GUID);
     public static int musicVolume = 100;
     
